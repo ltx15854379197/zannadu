e89d3f1876a618578fb9ddcc7b4abebaf4b8326e
ltx15854379197 <1443242057@qq.com>
2020-09-01 18:35:14 +0800

    鷹�111111

--- js/hotel.js
+++ js/hotel.js
@@ -24,34 +24,6 @@ $(function(){
             place.show();
 		}
 	})
-	$(".goTopBtn").click(function(){
-		$("html,body").animate({scrollTop:0},500)
-	})
-	var goTopBtn=$(".goTopBtn");
-    $(window).scroll(function(){
-        var h=$(document).scrollTop();
-        if(h>=1200){
-            goTopBtn.show();
-            goTopBtn.css({
-                "position":"fixed",
-                "top" :"675px",
-                "left":"50%",
-                "transform":"translate(-50%,0)"
-            })
-        }
-        if(h>6000){
-            goTopBtn.show();
-            goTopBtn.css({
-                "position":"relative",
-                "bottom":"0px",
-                "top":"5px",
-                "left":"20%"
-            })
-        }
-        if(h<1200){
-            goTopBtn.hide();
-        }
-    })
 // 轮模图
     var swiper = new Swiper('.picture .swiper-container', {
       spaceBetween: 30,
